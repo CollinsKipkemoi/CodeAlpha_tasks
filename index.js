6266e3dbe71eb0a47c0462ebf6e99a7e18ec9966
birkong1080 <birkong1080@gmail.com>
2024-07-01 12:42:40 +0000

    Feat: Add task, delete task

--- index.js
+++ index.js
@@ -1,38 +1,81 @@
 const sidebarItems = [
-    {
-        name: "Dashboard",
-        icon: "fas fa-th-large"
-    }, {
-        name: "All Tasks",
-        icon: "fa-solid fa-list-ul"
-    }, {
-        name: "Upcoming Tasks",
-        icon: "fa-solid fa-calendar-check"
-    }, {
-        name: "Completed Tasks",
-        icon: "fa-solid fa-list-check"
-    }, {
-        name: "Priorities",
-        icon: "fa-solid fa-ranking-star"
-    }
-]
+  {
+    name: "Dashboard",
+    icon: "fas fa-th-large",
+  },
+  {
+    name: "All Tasks",
+    icon: "fa-solid fa-list-ul",
+  },
+  {
+    name: "Upcoming Tasks",
+    icon: "fa-solid fa-calendar-check",
+  },
+  {
+    name: "Completed Tasks",
+    icon: "fa-solid fa-list-check",
+  },
+  {
+    name: "Priorities",
+    icon: "fa-solid fa-ranking-star",
+  },
+];
 
 const sidebarItemsDiv = document.querySelector(".sidebarItems");
-sidebarItems.forEach(item => {
-    const listItem = document.createElement("li");  
-    listItem.innerHTML = `<a href="#"><i class="${item.icon}"></i> ${item.name}</a>`;
-    sidebarItemsDiv.appendChild(listItem);
+sidebarItems.forEach((item) => {
+  const listItem = document.createElement("li");
+  listItem.innerHTML = `<a href="#"><i class="${item.icon}"></i> ${item.name}</a>`;
+  sidebarItemsDiv.appendChild(listItem);
 });
 
 // current month
-const month = new Date().toLocaleString('default', { month: 'long' });
+const month = new Date().toLocaleString("default", { month: "long" });
 const year = new Date().getFullYear();
 document.querySelector(".datepicker-top").innerHTML = `${month} ${year}`;
 const currentDate = new Date().getDate();
 const dates = document.querySelectorAll(".date");
-dates.forEach(date=> {
-    if(date.innerHTML == currentDate){
-        date.classList.add("current-day");
-        
-    }
-})
+dates.forEach((date) => {
+  if (date.innerHTML == currentDate) {
+    date.classList.add("current-day");
+  }
+});
+
+const tasks = [];
+const input = document.querySelector(".form-control");
+const addTask = document.querySelector("#addTask");
+const myTasks = document.querySelector(".my-tasks");
+const counter = document.querySelector(".count");
+
+addTask.addEventListener("click", () => {
+  const task = input.value;
+  if (task !== "") {
+    tasks.push(task);
+    input.value = "";
+    renderTasks();
+  }
+});
+
+function renderTasks() {
+  myTasks.innerHTML = "";
+  tasks.forEach((task, index) => {
+    const taskLi = document.createElement("li");
+    const taskContent = document.createElement("div");
+    taskContent.classList.add("taskContent");
+    const doneRadio = document.createElement("input");
+    doneRadio.setAttribute("type", "radio");
+    taskContent.appendChild(doneRadio);
+    const taskSpan = document.createElement("span");
+    taskSpan.textContent = task;
+    taskContent.appendChild(taskSpan);
+    const Xbutton = document.createElement("i");
+    Xbutton.setAttribute("class", "fa-solid fa-trash");
+    Xbutton.addEventListener("click", () => {
+      tasks.splice(index, 1);
+      renderTasks();
+    });
+    counter.innerHTML =  `(${tasks.length})` 
+    taskLi.appendChild(taskContent);
+    taskLi.appendChild(Xbutton);
+    myTasks.appendChild(taskLi);    
+  });
+}
